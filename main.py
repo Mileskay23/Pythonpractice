6b5f7c392e712b163c76478439c3fdc05f61d70d
Mileskay23 <820293@mystma.com>
2021-09-15 09:02:47 -0500

    stuff

---
+++ main.py
@@ -0,0 +1,23 @@
+def Topfivemovies():
+    #print the first movie  
+    print("Arrival")  
+
+    #print the second movie
+    print("Rampage")
+
+    #print third movie
+    print("Joker")
+
+    #print fourth movie
+    print("Moneyball")
+
+    #print fifth movie
+    print("A silent voice")
+
+print("these are my top five movies.")
+topFiveMovies()
+
+git config-global user.email "820293@mystma.com"
+git config-global user.name "Miles Kay"
+
+def 
