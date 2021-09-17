4b27274a4501583e56000eea725dcd207fcf8bca
Mileskay23 <820293@mystma.com>
2021-09-17 09:07:51 -0500

    madlib

--- main.py
+++ main.py
@@ -17,7 +17,42 @@ def Topfivemovies():
 print("these are my top five movies.")
 topFiveMovies()
 
-git config-global user.email "820293@mystma.com"
-git config-global user.name "Miles Kay"
+def Topfivefood():
+    #print first food
+    print("Taco")
 
-def 
+    #print second food
+    print("Pizza")
+
+    #print third food
+    print("Hot dogs")
+
+    #print fourth food
+    print("Chicken")
+
+    #print fifth food
+    print("Steak")
+
+print("these are my top five food")
+topFiveFood()
+
+def timesTwo(x):
+    print(x * 2)
+
+    timesTwo(30)
+
+def plusFive(x):
+    print(x + 5)
+
+    plusFive(10)
+
+def divideTwo(x):
+    print(x/2)
+
+    divideTwo(6)
+
+
+
+def madLib(big,story,big,planet,huge,fancy,ships,beautiful,immense,men,fancy,people,doctors,huge,dog,thank,silly,clean,ships,cleaners,wierd,use,grumpy):
+
+    print("star wars is a" + bigstory + "of" + big + "versus evil in a " + planet + "far far away. there are" + huge + "batttles between" + fancyships + "in" + beautiful + "space and" + immense + "duels with" + men + "called" + fancy + "sabers." + people + "called droidsd are helpers and" + doctors + "to the heroes. A" + huge + "power called the" + dogthank + "s people to do " + silly + "things, like" + cleanships + "the jedi" + cleaners + "use the force for the " + wierd + "side and the sith" + use + "it for the" + grumpy + "side")
